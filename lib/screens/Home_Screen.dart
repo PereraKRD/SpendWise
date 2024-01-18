3b53e3c8e995be82fed6917dde46ccc7f943d6df
RyanD <ryanperera0531@gmail.com>
2024-01-18 22:53:23 +0530

    last last one oenee

--- lib/screens/Home_Screen.dart
+++ lib/screens/Home_Screen.dart
@@ -39,48 +39,6 @@ class _HomeScreenState extends State<HomeScreen> {
         });
   }
 
-  confirmSignOut(BuildContext context) async {
-    return showDialog(
-      context: context,
-      builder: (context) {
-        return CupertinoAlertDialog(
-          title: Text('Confirm Sign Out'),
-          content: Text('Are you sure you want to Sign Out?'),
-          actions: <Widget>[
-            CupertinoDialogAction(
-              onPressed: () async {
-                Navigator.of(context).pop();
-                await performSignOut();
-              },
-              child: Text('Yes'),
-            ),
-            CupertinoDialogAction(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('No'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  performSignOut() async {
-    setState(() {
-      isLogoutLoader = true;
-    });
-    await authService.signOutUser();
-    Navigator.of(context).pushReplacement(
-      MaterialPageRoute(
-        builder: ((context) => const SignUpView()),
-      ),
-    );
-    setState(() {
-      isLogoutLoader = false;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -107,23 +65,6 @@ class _HomeScreenState extends State<HomeScreen> {
             child: HomeCreenAppBarText(
               userId: userID,
             )),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: IconButton(
-              onPressed: () {
-                confirmSignOut(context);
-              },
-              icon: isLogoutLoader
-                  ? CircularProgressIndicator()
-                  : Icon(
-                      Icons.logout,
-                      color: TColor.white,
-                      size: 30,
-                    ),
-            ),
-          ),
-        ],
       ),
       backgroundColor: TColor.bg,
       body: SingleChildScrollView(
